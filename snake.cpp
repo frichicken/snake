fb8fba64e2aa16e35c08ba46297a2bbc3b6388fe
nom nom <detu124@gmail.com>
2025-04-23 19:37:04 +0700

    Add project files.

---
+++ snake.cpp
@@ -0,0 +1,250 @@
+#include <string>
+#include <conio.h>
+#include <vector>
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+const int width = 30;
+const int height = 10;
+const char boundaryHorizontalSprite = '-';
+const char boundaryVerticalSprite = '|';
+const char foodSprite = 'o';
+const char snakeSprite = '*';
+const char tailSprite = '*';
+
+int score;
+
+enum class Direction
+{
+  IDLE,
+  UP,
+  LEFT,
+  RIGHT,
+  DOWN
+};
+
+class Position
+{
+public:
+  int x, y;
+  Position() {}
+  Position(int a, int b) : x(a), y(b) {}
+};
+
+class Snake
+{
+
+public:
+  Position position;
+  Direction direction;
+  vector<Position> tails;
+  Snake() {}
+} snake;
+
+class Food
+{
+public:
+  Position position;
+  Food() {};
+} food;
+
+void init();
+
+void ShowConsoleCursor(bool showFlag);
+void ClearScreen();
+
+int main()
+{
+  ShowConsoleCursor(false);
+  srand(time(0));
+  init();
+  char key = 0;
+  while (key != 27)
+  {
+    int tailLength = snake.tails.size();
+    for (int i = 0; i < height; i++)
+    {
+      for (int j = 0; j < width; j++)
+      {
+        if (i == 0 || i == height - 1)
+        {
+          cout << boundaryHorizontalSprite;
+        }
+        else if (j == snake.position.x && i == snake.position.y)
+        {
+          cout << snakeSprite;
+        }
+        else if (j == food.position.x && i == food.position.y)
+        {
+          cout << foodSprite;
+        }
+        else if (j == 0 || j == width - 1)
+        {
+          cout << boundaryVerticalSprite;
+        }
+        else
+        {
+          bool tail = false;
+          for (int n = 0; n < tailLength; n++)
+          {
+            if (i == snake.tails.at(n).y && j == snake.tails.at(n).x)
+            {
+              tail = true;
+              cout << tailSprite;
+            }
+          }
+
+          if (tail == false)
+          {
+            cout << " ";
+          }
+        }
+      }
+      cout << endl;
+    }
+
+    cout << "Score: " << score << endl;
+    cout << "Press R to restart" << endl;
+    cout << "Press ESC to exit" << endl;
+
+    cout << "[HEAD]" << "[" << snake.position.x << "]"
+         << "[" << snake.position.y << "]" << endl;
+    // for (int i = 0; i < tailLength; i++)
+    // {
+    //   cout << "[" << i << "]" << "[" << snake.tails[i].x << "]"
+    //        << "[" << snake.tails[i].y << "]" << endl;
+    // }
+
+    ClearScreen();
+
+    if (_kbhit())
+    {
+      key = _getch();
+      if ((key == 'W' || key == 'w') && snake.direction != Direction::DOWN)
+      {
+        snake.direction = Direction::UP;
+      }
+      else if ((key == 'A' || key == 'a') && snake.direction != Direction::RIGHT)
+      {
+        snake.direction = Direction::LEFT;
+      }
+      else if ((key == 'D' || key == 'd') && snake.direction != Direction::LEFT)
+      {
+        snake.direction = Direction::RIGHT;
+      }
+      else if ((key == 'S' || key == 's') && snake.direction != Direction::UP)
+      {
+        snake.direction = Direction::DOWN;
+      }
+      else if (key == 'R' || key == 'r')
+      {
+        init();
+      }
+    }
+
+    if (snake.position.x == food.position.x && snake.position.y == food.position.y)
+    {
+      score += 10;
+
+      food.position.x = rand() % (width - 2) + 1;
+      food.position.y = rand() % (height - 2) + 1;
+
+      Position postion;
+      if (tailLength == 0)
+      {
+        postion = snake.position;
+      }
+      else
+      {
+        postion = snake.tails[tailLength - 1];
+      }
+      snake.tails.push_back(postion);
+    }
+
+    Position preservedTail;
+
+    if (tailLength > 0)
+    {
+      preservedTail = snake.tails.at(0);
+      snake.tails.at(0) = snake.position;
+    }
+
+    for (int i = 1; i < tailLength; i++)
+    {
+      Position currentTail = snake.tails[i];
+      snake.tails[i] = preservedTail;
+      preservedTail = currentTail;
+    }
+
+    switch (snake.direction)
+    {
+    case Direction::UP:
+      snake.position.y--;
+      break;
+    case Direction::DOWN:
+      snake.position.y++;
+      break;
+    case Direction::LEFT:
+      snake.position.x--;
+      break;
+    case Direction::RIGHT:
+      snake.position.x++;
+      break;
+    default:
+      break;
+    }
+
+    for (auto tail : snake.tails)
+    {
+      if (tail.x == snake.position.x && tail.y == snake.position.y)
+      {
+        init();
+      }
+    }
+
+    if (snake.position.x > width - 1 || snake.position.y > height - 1 ||
+        snake.position.x < 0 || snake.position.y < 0)
+    {
+      init();
+    }
+
+    Sleep(50);
+  }
+}
+
+void init()
+{
+  ClearScreen();
+  food.position.x = rand() % (width - 2) + 1;
+  food.position.y = rand() % (height - 2) + 1;
+
+  snake.direction = Direction::IDLE;
+  snake.position.x = rand() % (width - 2) + 1;
+  snake.position.y = rand() % (height - 2) + 1;
+  snake.tails.clear();
+
+  score = 0;
+}
+
+void ShowConsoleCursor(bool showFlag)
+{
+  HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+
+  CONSOLE_CURSOR_INFO cursorInfo;
+
+  GetConsoleCursorInfo(out, &cursorInfo);
+  cursorInfo.bVisible = showFlag;
+  SetConsoleCursorInfo(out, &cursorInfo);
+}
+
+void ClearScreen()
+{
+  HANDLE hConsoleOutput;
+  COORD dwCursorPosition;
+
+  hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+  dwCursorPosition.X = 0;
+  dwCursorPosition.Y = 0;
+  SetConsoleCursorPosition(hConsoleOutput, dwCursorPosition);
+}
